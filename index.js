a345b226e5916ed3b3a2009bf16735599be7d5a5
waridrox <mohdwarid4@gmail.com>
2022-09-03 13:28:31 +0530

    Updated padid and api validation, stringified logs

--- index.js
+++ index.js
@@ -1,32 +1,37 @@
 'use strict';
 
+const fs = require('fs');
+const { URL } = require('url');
+const { debounce } = require('lodash');
 const axios = require("axios");
-const apikeyEtherpad = require('ep_etherpad-lite/node/handler/APIHandler').exportedForTestingOnly.apiKey;
 const api = require('ep_etherpad-lite/node/db/API');
 const db = require('ep_etherpad-lite/node/db/DB');
-const { URL } = require('url');
+const absolutePaths = require('ep_etherpad-lite/node/utils/AbsolutePaths');
+const apikey = fs.readFileSync(absolutePaths.makeAbsolute('APIKEY.txt')).toString();
 
-const { debounce } = require('lodash');
+const stringifyData = (data) => {
+  return JSON.stringify(data);
+};
 
 const getMetadata = async (context) => {
-  const getMetadata = await db.get(`efssmetadata:${context.pad.id}:${context.author}`).catch((err) => { console.error(err.message) })
+  const getMetadata = await db.get(`efssmetadata:${context.pad.id}:${context.author}`).catch((err) => { console.error(JSON.stringify(err.message)) });
 
-  console.log(getMetadata ? {code: 0, metadataFromDb: getMetadata} : {code: 1, metadataFromDb: getMetadata});
+  console.log(getMetadata ? stringifyData({code:0,metadataFromDb: getMetadata}) : stringifyData({code:0,metadataFromDb: getMetadata}));
   if (getMetadata) {
     const queryParams = getMetadata.split(':');
     const wopiSrc = decodeURIComponent(queryParams[0]);
     const wopiHost = new URL(wopiSrc).origin;
     const accessToken = queryParams[1];
-  
-    console.log({code: 0, metadataParams: {wopiSrc: wopiSrc}});
-  
+
+    console.log(stringifyData({code:0,metadataParams:{wopiSrc:`${wopiSrc} URL for serving requests to the WOPI server`}}));
+
     return [wopiHost, wopiSrc, accessToken];
   }
   else {
-    console.log({code: 0, metadataParams: null});
+    console.log(stringifyData({code:0,metadataParams:`metadata values for WOPI server fetched as null from db`}));
     return null;
   }
-}
+};
 
 const wopiCall = (wopiHost, wopiSrc, accessToken, padID, close=false) => {
   let axiosURL = `${wopiHost}/wopi/bridge/${padID}?WOPISrc=${wopiSrc}&access_token=${accessToken}`;
@@ -36,21 +41,20 @@ const wopiCall = (wopiHost, wopiSrc, accessToken, padID, close=false) => {
 
   axios.post(axiosURL)
   .then((response) => {
+    console.log(stringifyData(response));
     if (response.status === 200) {
-      console.log({code: 0, message: `${response.statusText}, Request executed successfully`});
+      console.log(stringifyData({code:0,message:`${response.statusText}, Request executed successfully`}));
     }
     if (response.status === 202) {
-      console.log({code: 0, message: `${response.statusText}, Enqueued action to the request`});
+      console.log(stringifyData({code:0,message:`${response.statusText}, Enqueued action to the request`}));
     }
   })
   .catch((error) => {
     if (error.status === 400 || error.status === 500) {
-      console.log({code: 0, message: `${error.statusText}. This form of request is denied`});
-      // lock the contents of the pad (by removing sessionID)
-      process.exit(1);
+      console.log(stringifyData({code:1,message:`${error.statusText}. This form of request is denied`}));
     }
     else {
-      console.log({code: 0, message: `${error.statusText}, Error occured while responding to the request`});
+      console.log(stringifyData({code:0,message:`Error occured while responding to the wopi request`}));
     }
   });
 };
@@ -67,54 +71,55 @@ const postToWopi = async (context) => {
 exports.setEFSSMetadata = async (hookName, context) => {
   context.app.post('/setEFSSMetadata', async (req, res) => {
     const query = req.query;
-    console.log({code: 0, query: query});
 
-    if (query.apikey === apikeyEtherpad) {
-      console.log('APIKEY FROM APIHANDLER FUNC: ', apikeyEtherpad, 'APIKEY FROM QUERY: ', query.apikey);
-  
-      const padList = await api.listAllPads();
-  
-      if (padList.padIDs.length == 0 || padList.padIDs.includes(query.padID)) {
+    console.log(stringifyData({code:0,query:query}));
+
+    if (query.apikey === apikey) {
+      const mapperToGroupExists = await db.get(`mapper2group:1`).catch((err) => { return null; })
+      const revisionCount = await api.getRevisionsCount(query.padID).catch((err) => { if (err.name === 'apierror') return null; });
+
+      if ((mapperToGroupExists && !revisionCount) || revisionCount) {
         if (!query.padID || !query.wopiSrc || !query.accessToken || !query.apikey)
-          res.send({code: 1, message: "Insufficient params or null values supplied as arguments!"});
+          res.send(stringifyData({code:0,message:"Insufficient params or null values supplied as arguments!"}));
         else {
           await db.set(`efssmetadata:${query.padID}`, `${(query.wopiSrc)}:${query.accessToken}`);
-          res.send({code: 0, message: "Content set successfully in db"});
+          res.send(stringifyData({code:0,message:"Content set successfully in db"}));
         }
+      }
+      else {
+        console.error('PadID is invalid');
+        res.send(stringifyData({code:1,message:"PadID is invalid"}));
       }
     }
 
     else {
-      console.error('Supplied PadID or API key in query is invalid');
+      console.error('Supplied API key is invalid');
+      res.send(stringifyData({code:1,message:"Supplied API key is invalid"}));
     }
   });
 };
 
 exports.padUpdate = debounce((hookName, context) => {
-  console.log({code: 0, message: `Pad content was updated after 3000 ms`});
+  console.log(stringifyData({code:0,message:`Pad content was updated after 3000 ms`}));
   postToWopi(context);
 }, 3000);
 
 exports.userJoin = async (hookName, {authorId, displayName, padId}) => {
-  console.log({code: 0, message: `${authorId} with DisplayName (${displayName}) joined pad ${padId}`});
 
   const dbkey = `efssmetadata:${padId}`;
   const dbval = await db.get(dbkey);
 
   if (dbval) {
     await db.set(`${dbkey}:${authorId}`, dbval);
-    console.log({code: 0, message: "Author metadata set successfully in db"});
+    console.log(stringifyData({code:0,message:'Pad author metadata set successfully in db'}));
     await db.remove(dbkey);
   }
   else {
     throw new Error("Author data doesn\'t exist");
   }
-}
+};
 
 exports.userLeave = function(hookName, session, callback) {
-
-  console.log({code: 0, message:`${session.author} left pad ${session.padId}`});
-
   const param = {
     author: session.author,
     pad: {
@@ -124,20 +129,18 @@ exports.userLeave = function(hookName, session, callback) {
 
   callback(new Promise(
     async (resolve, reject) => {
-
-      const metadata = await getMetadata(param).catch((err) => { console.error(err.message) });
-
+      const metadata = await getMetadata(param).catch((err) => { console.error(err) });
       if (metadata !== null) {
         const [wopiHost, wopiSrc, accessToken] = metadata;
         wopiCall(wopiHost, wopiSrc, accessToken, session.padId, true);
         await db.remove(`efssmetadata:${session.padId}:${session.author}`);
 
-        resolve(console.log({code: 0, message:`Exited author content removed successfully from db`}));
+        resolve(console.log(stringifyData({code:0,message:`Exited author content removed successfully from db`})));
       }
       else {
-        reject(console.log({code: 0, message: `Author data doesn\'t exist`}));
+        reject(console.error(stringifyData({code:0,message:`Author data doesn\'t exist`})));
       }
     }
   ))
   return;
-}
+};
