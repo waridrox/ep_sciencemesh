f67c9c80171869225a35de64b6fdbff9236a29ce
waridrox <mohdwarid4@gmail.com>
2022-09-12 22:58:42 +0530

    Handling requests via custom client handler function

--- static/js/index.js
+++ static/js/index.js
@@ -2,36 +2,46 @@
 
 const padModals = require('ep_etherpad-lite/static/js/pad_modals.js').padmodals;
 
-const padModalShow = (message, delay) => {
-  $("#notification-popup-id p").text(message);
-  $('#notification-popup-id h2').addClass('notify_success').text("Success");
+const padModalShow = (message, delay, status) => {
+  let notificationPopupId = "#notification-popup-id";
+  $(`${notificationPopupId} p`).text(message);
+  $(`${notificationPopupId} h2`).text("Success");
 
   padModals.showModal("ep_sciencemesh-popup");
   padModals.hideOverlay();
 
-  $("#notification-popup-id").delay(`${delay*1000}`).fadeOut('slow');
+  // style classes get overwritten at handleClientMessage_CUSTOM
+  if (status >= 200 && status < 400) $('#notification-popup-id h2').addClass('notify_success');
+  else if (status >= 400) $('#notification-popup-id h2').addClass('notify_error');
+
+  if (delay === 0) {
+    $("#connectivity").click(() => $('#connectivity').hide());
+  }
+  else {
+    $("#connectivity").delay(`${delay*1000}`).fadeOut('slow');
+  }
 }
 
-exports.handleClientMessage_COLLABROOM = (hook, context, cb) => {
-  console.log(context);
+exports.handleClientMessage_CUSTOM = (hook, context, cb) => {
+  if (context.payload.action === 'recieveNotificationMessage') {
 
-  let notification = JSON.parse(notificationData);
-  if (Object.keys(notification).length === 3) {
-    let { message, delay, status } = notification;
+    const message = context.payload.message;
+    if (message) {
+      let notification = message;
 
-    if (status >= 200 && status < 400) {
-      $('#notification-popup-id h2').addClass('notify_success').text("Success");;
-      padModalShow(message, delay);
+      if (Object.keys(notification).length === 3) {
+          let { message, delay, status } = notification;
+
+          if (status >= 200 && status < 400) {
+            $('#notification-popup-id h2').text("Success");
+            padModalShow(message, delay, status);
+          }
+          else if (status >= 400) {
+            $('#notification-popup-id h2').text("Error");
+            padModalShow(message, delay, status);
+          }
+        }
+      }
     }
-    else if (status >= 400) {
-      $('#notification-popup-id h2').addClass('notify_error').text("Error");;
-      padModalShow(message, delay);
-    }
-  }
   cb(null);
-};
-
-exports.documentReady = () => {
-  console.log('hi from static assets')
-  padModalShow("test_message", 15);
 };
